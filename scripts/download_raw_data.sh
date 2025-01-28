ecd072f8a14ed7404b92fe6682ee8f3d92619304
Malachi Griffith <malachig@gmail.com>
2025-01-28 10:55:32 -0600

    update download data script

---
+++ scripts/download_raw_data.sh
@@ -0,0 +1,35 @@
+cd /storage1/fs1/mgriffit/Active/griffithlab/adhoc/ratner_p01/htlv_integration_sites/fastqs/
+
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_CTCF-7_SIC_934_SIC2_Ratner_196_CGTATCTCA_AATACTAATA_S2_I1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_CTCF-7_SIC_934_SIC2_Ratner_196_CGTATCTCA_AATACTAATA_S2_I2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_CTCF-7_SIC_934_SIC2_Ratner_196_CGTATCTCA_AATACTAATA_S2_R1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_CTCF-7_SIC_934_SIC2_Ratner_196_CGTATCTCA_AATACTAATA_S2_R2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_CTCF-8_SIC_935_SIC2_Ratner_196_GTCCTGCCG_AATACTAATA_S3_I1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_CTCF-8_SIC_935_SIC2_Ratner_196_GTCCTGCCG_AATACTAATA_S3_I2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_CTCF-8_SIC_935_SIC2_Ratner_196_GTCCTGCCG_AATACTAATA_S3_R1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_CTCF-8_SIC_935_SIC2_Ratner_196_GTCCTGCCG_AATACTAATA_S3_R2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_P12-10B_SIC_936_SIC2_Ratner_196_CCGGGACAC_AATACTAATA_S4_I1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_P12-10B_SIC_936_SIC2_Ratner_196_CCGGGACAC_AATACTAATA_S4_I2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_P12-10B_SIC_936_SIC2_Ratner_196_CCGGGACAC_AATACTAATA_S4_R1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_P12-10B_SIC_936_SIC2_Ratner_196_CCGGGACAC_AATACTAATA_S4_R2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_P12-14_SIC_937_SIC2_Ratner_196_GGCTGGGAT_AATACTAATA_S5_I1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_P12-14_SIC_937_SIC2_Ratner_196_GGCTGGGAT_AATACTAATA_S5_I2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_P12-14_SIC_937_SIC2_Ratner_196_GGCTGGGAT_AATACTAATA_S5_R1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-393/Ratner_P12-14_SIC_937_SIC2_Ratner_196_GGCTGGGAT_AATACTAATA_S5_R2_001.fastq.gz
+
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_CTCF-1_SIC_934_196_CGTATCTCAA_AATACTAATA_S156_I1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_CTCF-1_SIC_934_196_CGTATCTCAA_AATACTAATA_S156_I2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_CTCF-1_SIC_934_196_CGTATCTCAA_AATACTAATA_S156_R1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_CTCF-1_SIC_934_196_CGTATCTCAA_AATACTAATA_S156_R2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_CTCF-3_SIC_935_196_GTCCTGCCGA_AATACTAATA_S157_I1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_CTCF-3_SIC_935_196_GTCCTGCCGA_AATACTAATA_S157_I2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_CTCF-3_SIC_935_196_GTCCTGCCGA_AATACTAATA_S157_R1_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_CTCF-3_SIC_935_196_GTCCTGCCGA_AATACTAATA_S157_R2_001.fastq.gz
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_P12-5_SIC_936_196_CCGGGACACA_AATACTAATA_S158_I1_001.fastq.gz 
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_P12-5_SIC_936_196_CCGGGACACA_AATACTAATA_S158_I2_001.fastq.gz 
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_P12-5_SIC_936_196_CCGGGACACA_AATACTAATA_S158_R1_001.fastq.gz 
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_P12-5_SIC_936_196_CCGGGACACA_AATACTAATA_S158_R2_001.fastq.gz 
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_P12-8_SIC_937_196_GGCTGGGATA_AATACTAATA_S159_I1_001.fastq.gz 
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_P12-8_SIC_937_196_GGCTGGGATA_AATACTAATA_S159_I2_001.fastq.gz 
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_P12-8_SIC_937_196_GGCTGGGATA_AATACTAATA_S159_R1_001.fastq.gz 
+wget https://dsildata.htcf.wustl.edu/Ratner-82411703/1173-421/Ratner_P12-8_SIC_937_196_GGCTGGGATA_AATACTAATA_S159_R2_001.fastq.gz 
